82aa6b5383dad6bdc3b18eae9f6d616a86686c0e
rsupp123 <rudo.supple@gmail.com>
2018-08-15 10:17:49 +0100

    Update ecoli_queries.sql

--- ecoli_queries.sql
+++ ecoli_queries.sql
@@ -1,3 +1,7 @@
+/*sample queries for database.
+retrieves taxid, scientific name and genbank genes whose number of gb genes are < 5000 and the genus is shigella
+*/
+
 SELECT taxid, sci_name, gb_genes 
 FROM taxonomy 
 LEFT JOIN organism 
@@ -5,8 +9,9 @@ ON bpid=bio_id
 WHERE gb_genes < 5000 
 AND genus = 'Shigella';
 
-
-
+/*
+retrieves BPID, taxid and genbank genes and scientific name with more than one taxid i.e. retrieves BPIDs sharing same taxid
+*/
 
 SELECT bio_id, taxid, gb_genes, sci_name 
 FROM taxonomy 
